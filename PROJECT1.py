48a88ffc1300a5504798bca0791c8a28d5799825
PLZ ENTER TEXT <75538472+PLZENTERTEXT@users.noreply.github.com>
2021-07-04 22:57:06 +0800

    Add files via upload

---
+++ PROJECT1.py
@@ -0,0 +1,13 @@
+#Program to generate MD5 of string data
+import hashlib
+
+string = str(input("Please enter your input: "))
+
+encoded = string.encode()
+#encode() converts the string into bytes to be acceptable by hash function
+result = hashlib.md5(encoded)
+
+print("Your Input: ", string)
+print("Hash Value : ", result)
+print("Hexadecimal Equivalent : ", result.hexdigest())
+#hexdigest() for hexadecimal format, digest() for byte format
